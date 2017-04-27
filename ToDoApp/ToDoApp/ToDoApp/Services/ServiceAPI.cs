7c67b481bdabcb34bb9bae71aea41d64b4795618
Fernando Arreguin Leija <farreguin@outlook.com>
2017-04-26 22:32:35 -0500

    Version Final del Evento

---
+++ ToDoApp/ToDoApp/ToDoApp/Services/ServiceAPI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ToDoApp.Services
+{
+    public class ServiceAPI
+    {
+        public static ServiceAPI Methods => new ServiceAPI();
+        public const string urlapi = "http://webapixamarin.azurewebsites.net/api/task";
+        public async Task<List<Task>> GetAllTask()
+        {
+            List<Task> LstTask = new List<Task>();
+            try
+            {
+                //HttpClient client;
+                //var Response = await client.GetAsync(BaseUrl + "state/getAll", HttpCompletionOption.ResponseHeadersRead);
+                //ProcessResponse(Response);
+                //await Response.Content.ReadAsStringAsync().ContinueWith((readTask) =>
+                //{
+                //    result = JsonConvert.DeserializeObject<List<StateViewModel>>(readTask.Result);
+                //});
+            }
+            catch(Exception ex)
+            {
+                    
+            }
+            return LstTask;
+        }
+    }
+}
--- WebAPI/WebAPI/App_Start/FilterConfig.cs
+++ WebAPI/WebAPI/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace WebAPI
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
